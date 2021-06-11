4e0c8dbc89882db1971bb3fc9ccd544643a626de
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2021-06-10 21:04:48 -0300

    feat: deploy to heroku

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -1,12 +1,12 @@
 import { ConnectionOptions } from "typeorm";
 
 export default {
-   type: "postgres",
+   type: "sqlite",
    host: process.env.DB_HOST,
    port: parseInt(process.env.DB_PORT),
    username: process.env.DB_USERNAME,
    password: process.env.DB_PASSWORD,
-   database: process.env.DB_NAME,
+   database: process.env.DB_NAME || "db.sqlite",
    synchronize: true,
    logging: false,
    ssl: {
--- backend/src/migration/1623322272662-coupon.ts
+++ backend/src/migration/1623322272662-coupon.ts
@@ -1,14 +1,21 @@
-import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 import { Coupon } from "../entity/Coupon";
 
 export class coupon1623322272662 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.addColumn(
+      "coupon",
+      new TableColumn({
+        name: "rarity",
+        default: "common",
+        enum: ["common", "rare", "ultrarare"],
+        isNullable: false,
+        type: "string",
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.addColumn("Coupon", new TableColumn({name: "rarity", default: "common", enum: ["common", "rare", "ultrarare"], isNullable: false, type: "string"}))
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.dropColumn("Coupon", "rarity")
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropColumn("coupon", "rarity");
+  }
 }
